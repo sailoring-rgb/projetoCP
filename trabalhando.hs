6a63fb05b24850b860510efbfe33d054d58a9e97
Ana Henriques <62114404+AnaHenriques01@users.noreply.github.com>
2021-05-30 09:58:53 +0100

    Update trabalhando.hs

--- trabalhando.hs
+++ trabalhando.hs
@@ -1,3 +1,11 @@
-import BTree.hs
+inExpAr :: Either b (Either a (Either (BinOp, (ExpAr a, ExpAr a)) (UnOp, ExpAr a))) -> ExpAr a
+inExpAr = [X , num ops]
+		where
+		num ops = [N,ops]
+		ops = [bin,Un]
+		bin (op,(a,b)) = Bin op a b
 
+baseExpAr f g h j k l z = f + (g + (h × (j × k ) + l × z))
 
+outExpAr :: ExpAr a -> Either () (Either a (Either (BinOp, (ExpAr a, ExpAr a)) (UnOp, ExpAr a)))
+outExpAr = 
